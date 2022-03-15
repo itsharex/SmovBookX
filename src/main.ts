14384162f28006b556701b7d5686cf5fc05be884
leri <dw99599@163.com>
2022-03-15 15:46:26 +0800

    添加了 操蛋的 按需引入

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,14 @@
 import { createApp } from 'vue'
 import App from './layout/index.vue'
-import ElementPlus from 'element-plus'
-import 'element-plus/dist/index.css'
 import router from "./router/router";
 import VXETable from 'vxe-table'
 import 'vxe-table/lib/style.css'
 import "./global_event"
-import devtools from '@vue/devtools'
 
+// import devtools from '@vue/devtools'
 
-if (process.env.NODE_ENV === 'development') {
-  devtools.connect();
-  // "localhost", 3000
-}
+// if (process.env.NODE_ENV === 'development') {
+//   devtools.connect();
+// }
 
-
-createApp(App).use(ElementPlus).use(router).use(VXETable).mount('#app');
+createApp(App).use(router).use(VXETable).mount('#app');
--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,24 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 
 export default defineConfig({
-  plugins: [vue(),],
+  plugins: [
+    vue(),
+    AutoImport({
+      resolvers: [ElementPlusResolver()],
+    }),
+    Components({
+      extensions: ['vue', 'md'],
+      include: [/\.vue$/, /\.vue\?vue/, /\.md$/],
+      resolvers: [
+        ElementPlusResolver(),
+      ],
+      dts: 'src/components.d.ts',
+    }),
+  ],
   server: {
     hmr: { overlay: false },
   },
