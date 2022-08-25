ad2b53b337fca603305eef9bcd34219e7c96f38d
leri <214zzl995@gmail.com>
2022-08-25 14:28:16 +0800

    一些修改

--- src-tauri/src/hfs/api.rs
+++ src-tauri/src/hfs/api.rs
@@ -1,7 +1,10 @@
-use super::{handle::file_handle_error, smov_method};
+use super::{
+  handle::{file_handle_error, render_not_found},
+  smov_method,
+};
 use axum::{
   routing::{get, get_service},
-  Router, response::IntoResponse,
+  Router,
 };
 use tower_http::services::ServeDir;
 
@@ -14,14 +17,9 @@ pub fn system_api() -> Router {
 pub fn file_api() -> Router {
   let tidy_folder = &crate::app::APP.lock().clone().conf.tidy_folder;
   //通过阅读代码 解决办法在 https://github.com/search?l=Rust&p=2&q=not_found_service&type=Code 应该要在 tower_http 设置not_found_fallback 即ServeDir::new(tidy_folder) 部分
-  let svc = get_service(
-    ServeDir::new(tidy_folder).not_found_service(tower::service_fn(
-      |request: axum::http::Request<_>| async move {
-        std::io::Result::Ok(format!("{} not found", request.uri()).into_response())
-      },
-    )),
-  )
-  .handle_error(file_handle_error);
+  let svc =
+    get_service(ServeDir::new(tidy_folder).not_found_service(tower::service_fn(render_not_found)))
+      .handle_error(file_handle_error);
   Router::new().nest("/", svc)
 }
 
@@ -29,4 +27,5 @@ pub fn data_api() -> Router {
   Router::new()
     .route("/all", get(smov_method::get_data_all))
     .route("/pagination", get(smov_method::get_data_pagination))
+    .route("/error_test", get(smov_method::error_test))
 }
--- src-tauri/src/hfs/handle.rs
+++ src-tauri/src/hfs/handle.rs
@@ -2,8 +2,9 @@ use std::io;
 
 use axum::{
   http::StatusCode,
-  response::{Html, IntoResponse, Response},
+  response::{ IntoResponse, Response},
 };
+
 
 pub async fn file_handle_error(_err: io::Error) -> impl IntoResponse {
   (StatusCode::INTERNAL_SERVER_ERROR, "Something went wrong...")
@@ -13,12 +14,6 @@ pub async fn handler_404() -> impl IntoResponse {
   (StatusCode::NOT_FOUND, "nothing to see here")
 }
 
-// pub fn render_not_found() -> impl IntoResponse {
-//   tower::service_fn(
-//     |request: axum::http::Request<_>| async move {
-//       std::io::Result::Ok(format!("{} not found", request.uri()).into_response())
-//     },
-//   )
-// }
-
-
+pub async fn render_not_found<T>(request: axum::http::Request<T>) -> std::io::Result<Response> {
+  std::io::Result::Ok(format!("{} not found", request.uri()).into_response())
+}
